79db28791fdb7c09b6ce001451185ccc14b9d8a6
Sean.Mars <mars0222@gmail.com>
2019-02-21 00:24:23 +0800

    增加文字相關測試

--- canvas/test.js
+++ canvas/test.js
@@ -1,4 +1,20 @@
-const glob = require('glob');
+const Jimp = require('jimp');
 
-let files = glob.sync('./images/full-*.jpg');
-console.log(files.length);
+async function run() {
+    let fontPath = './images/font.fnt';
+
+    let img = await new Jimp(78, 18, '#ffffff');
+
+    new Jimp(img, async (err, image) => {
+        const font = await Jimp.loadFont(fontPath);
+        image.print(font, 0, 0, 'ABC');
+        await image.writeAsync('./test.png');
+    });
+
+    new Jimp(img, async (err, image) => {
+        await image.setPixelColor(Jimp.rgbaToInt(255, 0, 0, 255), 0, 0);
+        await image.writeAsync('./test2.png');
+    });
+}
+
+run();
