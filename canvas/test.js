0088a8f384fc04ad434dff3ee7d6de6d23af1ed9
Sean.Mars <mars0222@gmail.com>
2019-02-22 01:02:27 +0800

    update test

--- canvas/test.js
+++ canvas/test.js
@@ -1,20 +1,30 @@
 const Jimp = require('jimp');
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function run() {
-    let fontPath = './images/font.fnt';
+    // let fontPath = './images/font.fnt';
 
-    let img = await new Jimp(78, 18, '#ffffff');
+    // let img = await new Jimp(78, 18, '#ffffff');
 
-    new Jimp(img, async (err, image) => {
-        const font = await Jimp.loadFont(fontPath);
-        image.print(font, 0, 0, 'ABC');
-        await image.writeAsync('./test.png');
-    });
+    // new Jimp(img, async (err, image) => {
+    //     const font = await Jimp.loadFont(fontPath);
+    //     image.print(font, 0, 0, 'ABC');
+    //     await image.writeAsync('./test.png');
+    // });
 
-    new Jimp(img, async (err, image) => {
-        await image.setPixelColor(Jimp.rgbaToInt(255, 0, 0, 255), 0, 0);
-        await image.writeAsync('./test2.png');
-    });
+    // new Jimp(img, async (err, image) => {
+    //     await image.setPixelColor(Jimp.rgbaToInt(255, 0, 0, 255), 0, 0);
+    //     await image.writeAsync('./test2.png');
+    // });
+
+    console.log(1);
+    let img = await Jimp.read('./test.png');
+    console.log(2);
+    await sleep(500);
+    console.log(3);
 }
 
 run();
